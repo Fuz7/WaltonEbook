c363c65a929b7d570b51aa32be4759b1644af22c
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2024-02-26 07:20:50 +0800

    Add account page fxml, css, controller

---
+++ src/main/java/org/openjfx/program/controller/accountPageController.java
@@ -0,0 +1,11 @@
+package org.openjfx.program.controller;
+
+public class accountPageController {
+
+    public mainPageController mainPageController;
+
+    public void setMainPageController(mainPageController mainPageController) {
+        this.mainPageController = mainPageController;
+    }
+
+}
